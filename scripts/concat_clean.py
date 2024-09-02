9ab1c236257037cbb27f7d28790ee02827709ef8
Matthieu COLIN <matthieu.cln@gmail.com>
2024-09-02 16:52:26 +0200

    Visualisations

--- scripts/concat_clean.py
+++ scripts/concat_clean.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 in_relative_path = "../voyages_covoit_fr/exports_covoit/"
 in_absolute_path = os.path.abspath(os.path.join(os.getcwd(), in_relative_path))
-out_relative_path = "../voyages_covoit_fr/outputs/reduced_concat_covoit.csv"
+out_relative_path = "../voyages_covoit_fr/outputs/reduced_concat_covoit_full.csv"
 out_absolute_path = os.path.abspath(os.path.join(os.getcwd(), out_relative_path))
 out_absolute_folder_path = "/".join(out_absolute_path.split("/")[:-1])
 
